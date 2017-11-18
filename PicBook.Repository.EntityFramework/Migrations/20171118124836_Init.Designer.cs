6e0975ab41d759b118518d71b122c5043fe778a8
imredobos <dobos.imre95@gmail.com>
2017-11-18 14:09:22 +0100

    remove jsons

--- PicBook.Repository.EntityFramework/Migrations/20171118124836_Init.Designer.cs
+++ PicBook.Repository.EntityFramework/Migrations/20171118124836_Init.Designer.cs
@@ -11,14 +11,14 @@ using System;
 namespace PicBook.Repository.EntityFramework.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20171118113840_ImageMetadataInit")]
-    partial class ImageMetadataInit
+    [Migration("20171118124836_Init")]
+    partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.0.0-rtm-26452")
+                .HasAnnotation("ProductVersion", "2.0.1-rtm-125")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("PicBook.Domain.PictureEntity", b =>
--- PicBook.Web/Controllers/AccountController.cs
+++ PicBook.Web/Controllers/AccountController.cs
@@ -1,14 +1,10 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PicBook.Web.Models;
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using PicBook.ApplicationService;
-using PicBook.Repository.EntityFramework;
 
 namespace PicBook.Web.Controllers
 {
